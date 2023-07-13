e37138ee785ac280835192b1e557cc5da0a8856d
podkrecam <podkrecam@gmail.com>
2023-07-13 21:42:48 +0200

    addedComments

--- src/String2/SameStarChar.java
+++ src/String2/SameStarChar.java
@@ -1,6 +1,9 @@
 package String2;
 
 public class SameStarChar {
+    /*
+    Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
+     */
     private String str;
 
     public SameStarChar(String str) {
@@ -13,8 +16,10 @@ public class SameStarChar {
         int starHasChars = 0;
 
         for (int i = 1; i < str.length() - 1; i++) {
+            // check if char at index is *
             if (str.charAt(i) == '*') {
                 countStars++;
+                // if char at index i is * check if the char before and after are the same
                 if (str.charAt(i - 1) == str.charAt(i + 1)) {
                     starHasChars++;
                 }
