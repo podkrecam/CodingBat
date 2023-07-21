609dcfd146b4c338c49935561aaa8304b65ee854
podkrecam <podkrecam@gmail.com>
2023-07-21 19:46:38 +0200

    update

--- src/Array2/MainArray2.java
+++ src/Array2/MainArray2.java
@@ -7,5 +7,7 @@ public class MainArray2 {
         BigDiff bigDiff = new BigDiff(arrayCountEvens);
         int[] centeredAverageArray = {1, 1, 99, 99};
         CenteredAverage centeredAverage = new CenteredAverage(centeredAverageArray);
+        int[] sum13Array = {1, 2, 2, 1, 13};
+        Sum13 sum13 = new Sum13(sum13Array);
     }
 }
---
+++ src/Array2/Sum13.java
@@ -0,0 +1,33 @@
+package Array2;
+
+public class Sum13 {
+    /*
+Return the sum of the numbers in the array, returning 0 for an empty array.
+Except the number 13 is very unlucky, so it does not count and numbers that
+come immediately after a 13 also do not count.
+
+sum13([1, 2, 2, 1]) → 6
+sum13([1, 1]) → 2
+sum13([1, 2, 2, 1, 13]) → 6
+     */
+    private int[] array;
+
+    public Sum13(int[] array) {
+        this.array = array;
+        System.out.println("Sum13: " + sum());
+    }
+
+    private int sum() {
+        int sum = 0;
+        if (array.length > 0) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != 13) {
+                    sum += array[i];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return sum;
+    }
+}
