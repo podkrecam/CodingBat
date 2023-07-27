5285d182904d616ae9b2e0e1cd8373faf5efe2e0
podkrecam <podkrecam@gmail.com>
2023-07-27 23:18:07 +0200

    update

--- src/Logic2/MainLogic2.java
+++ src/Logic2/MainLogic2.java
@@ -3,6 +3,7 @@ package Logic2;
 public class MainLogic2 {
     public static void main(String[] args) {
         MakeBricks makeBricks = new MakeBricks(20, 0, 19);
-        LoneSum loneSum = new LoneSum(9,2,2);
+        LoneSum loneSum = new LoneSum(9, 2, 2);
+        LuckySum luckySum = new LuckySum(1, 2,13);
     }
 }
