8ac843eb14279d3e152afc962ac1df06e5dbe535
podkrecam <podkrecam@gmail.com>
2023-07-30 02:58:09 +0200

    update

--- src/String3/MainString3.java
+++ src/String3/MainString3.java
@@ -3,5 +3,6 @@ package String3;
 public class MainString3 {
     public static void main(String[] args) {
         CountYZ countYZ = new CountYZ("!!day--yaz!!");
+        WithoutString withoutString = new WithoutString("Hello there", "llo");
     }
 }
---
+++ src/String3/WithoutString.java
@@ -0,0 +1,33 @@
+package String3;
+
+public class WithoutString {
+    /*
+    Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+
+    withoutString("Hello there", "llo") → "He there"
+    withoutString("Hello there", "e") → "Hllo thr"
+    withoutString("Hello there", "x") → "Hello there"
+     */
+    private String base;
+    private String remove;
+
+    public WithoutString(String base, String remove) {
+        this.base = base;
+        this.remove = remove;
+        System.out.println("WithoutString: " + create());
+    }
+
+    private String create() {
+        String temp = "";
+
+        for (int i = 0; i < base.length(); i++) {
+            if (i <= base.length() - remove.length() && base.substring(i, i + remove.length()).equalsIgnoreCase(remove)) {
+                i += remove.length() - 1;
+            } else {
+                temp += base.charAt(i);
+            }
+        }
+
+        return temp;
+    }
+}
